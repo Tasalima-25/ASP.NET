16ed41109aecea13ed7f7c8fe1739a7feb9c68f9
Tasalima Jamadar <143584159+Tasalima-25@users.noreply.github.com>
2026-04-27 11:43:40 +0530

    Add files via upload

---
+++ mvc.cs
@@ -0,0 +1,18 @@
+/*
+MVC : model view control
+retaurant system real time example
+model making dish
+view order customer
+control veter
+model : it is data layer
+        represents data and business logic : database interactions data validations
+view :  it is UI layer
+        represents UI what user see on browser
+        responsibilities : form tables and all
+controller : it is brain of MVC 
+            handles user requests and application flow 
+            connects model and view
+
+
+*/
+
